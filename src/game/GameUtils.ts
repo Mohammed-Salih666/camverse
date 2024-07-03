d344c8c5904cbe6177b4676d0b3ebe38ec2ffe80
RidhaMuneer <ridhamuner@gmail.com>
2024-07-03 22:21:18 +0300

    added the speed as a variable so that it can be changed in on place

--- src/game/GameUtils.ts
+++ src/game/GameUtils.ts
@@ -2,19 +2,20 @@
 import { GameObj } from "kaplay";
 
 export default class GameUtils {
+  static CHARACTER_SPEED = 1000;
   static moveRight(object: GameObj) {
-    object.move(1000, 0);
+    object.move(GameUtils.CHARACTER_SPEED, 0);
   }
 
   static moveLeft(object: GameObj) {
-    object.move(-1000, 0);
+    object.move(-GameUtils.CHARACTER_SPEED, 0);
   }
 
   static moveUp(object: GameObj) {
-    object.move(0, -1000);
+    object.move(0, -GameUtils.CHARACTER_SPEED);
   }
 
   static moveDown(object: GameObj) {
-    object.move(0, 1000);
+    object.move(0, GameUtils.CHARACTER_SPEED);
   }
 }
